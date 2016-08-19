2cba8e14470a9c253e29f1bec3d97560caacf9db
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2016-08-19 14:39:40 +0300

    perform check in a correct way

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 module.exports = log;
 
 function log () {
-    if (! performCheck) return false;
+    if (! performCheck()) return false;
     return window.console.log.apply(window.console, arguments);
 }
 
 log.log = log;
 
 log.error = function () {
-    if (! performCheck) return false;
+    if (! performCheck()) return false;
     return window.console.error.apply(window.console, arguments);
 };
 
 log.warn = function () {
-    if (! performCheck) return false;
+    if (! performCheck()) return false;
     return window.console.warn.apply(window.console, arguments);
 };
 
 log.debug = function () {
-    if (! performCheck) return false;
+    if (! performCheck()) return false;
     return window.console.debug.apply(window.console, arguments);
 }
 
 log.info = function () {
-    if (! performCheck) return false;
+    if (! performCheck()) return false;
     return window.console.info.apply(window.console, arguments);
 }
 
