29388832d3f1c31e2b69e60a989fff3db34fb2d1
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2016-08-19 14:28:02 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = log;
+
+function log () {
+    if (! performCheck) return false;
+    return window.console.log.apply(window.console, arguments);
+}
+
+log.log = log;
+
+log.error = function () {
+    if (! performCheck) return false;
+    return window.console.error.apply(window.console, arguments);
+};
+
+log.warn = function () {
+    if (! performCheck) return false;
+    return window.console.warn.apply(window.console, arguments);
+};
+
+log.debug = function () {
+    if (! performCheck) return false;
+    return window.console.debug.apply(window.console, arguments);
+}
+
+log.info = function () {
+    if (! performCheck) return false;
+    return window.console.info.apply(window.console, arguments);
+}
+
+////////
+
+function performCheck () {
+    if (! window.WP_DEBUG) return false;
+    if (! window.console) return false;
+}
+
